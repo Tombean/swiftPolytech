affafe71441dc9860381659478933e78aa22ec33
Laure MARCHAL <laure.marchal@igtp4-mac01.isim.intra>
2017-02-13 09:19:56 +0100

    Initial Commit

---
+++ ZboobeeTests/ZboobeeTests.swift
@@ -0,0 +1,36 @@
+//
+//  ZboobeeTests.swift
+//  ZboobeeTests
+//
+//  Created by Laure MARCHAL on 13/02/2017.
+//  Copyright © 2017 Laure MARCHAL. All rights reserved.
+//
+
+import XCTest
+@testable import Zboobee
+
+class ZboobeeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
