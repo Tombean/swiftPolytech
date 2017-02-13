88af9f3cc0613ce883466361b6a6f295e7a22cc4
Tom SOMERVILLE-ROBERTS <tom.somerville-rober@igtp4-mac01.isim.intra>
2017-02-13 13:41:52 +0100

    ADD : - Label / PickerView / Text field on home page.

--- Zboobee/ViewController.swift
+++ Zboobee/ViewController.swift
@@ -8,18 +8,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
+    @IBOutlet weak var channelPicker: UIPickerView!
+    @IBOutlet weak var messageToPost: UITextField!
+    
+    // Temporary :  needs to be changed with channel allowed from DB
+    let pickerData = ["All","Class","Class - Teachers","Class - Office"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.channelPicker.dataSource = self
+        self.channelPicker.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    //MARK: - Delegates and data sources
+    //MARK: Data Sources
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    //MARK: Delegates
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
 
 }
 
